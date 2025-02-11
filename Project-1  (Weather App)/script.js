bb96784ba28078c1718f032e7ec39f9c13d6ecfb
mohamed hassan <141786275+mohamedhassann27@users.noreply.github.com>
2025-02-12 01:34:39 +0200

    Update script.js

--- Project-1  (Weather App)/script.js
+++ Project-1  (Weather App)/script.js
@@ -1,41 +1,43 @@
-
-const apiKey="0d98bf40ef0d8147790f03012a301755"
+const apiKey="08e37ea34ad40e45e034f25064c6dc92"
 const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
-async function checkWeather(city){ 
-    const response= await fetch(apiUrl + city + `&appid=${apiKey}`)
+let checkWeather= async function(city){ 
 
-    if(response.status==404){
-        document.querySelector(".error").style.display="block"
-        document.querySelector(".weather").style.display="none"
+    try{
+        let response= await fetch(apiUrl + city + `&appid=${apiKey}`);
+        if(response.status==404){
+            document.querySelector(".error").style.display="block"
+            document.querySelector(".weather").style.display="none"
+        }
+        else{
+            let data=await response.json()
+            console.log(data);
+    
+            document.querySelector(".city").innerText=data.name;
+            document.querySelector(".temp").innerText=Math.round(data.main.temp)+"℃"
+            document.querySelector(".humidity").innerText=data.main.humidity+"%"
+            document.querySelector(".wind").innerText=data.wind.speed+ " km/h"
+            document.querySelector(".weatherIcon").src=`./images/${data.weather[0].main}.png`
+            document.querySelector(".weather").style.display="block"
+            document.querySelector(".error").style.display="none"
+        }
     }
-    else{
-        let data=await response.json()
-        console.log(data);
-
-        document.querySelector(".city").innerText=data.name
-        document.querySelector(".temp").innerText=Math.round(data.main.temp)+"℃"
-        document.querySelector(".humidity").innerText=data.main.humidity+"%"
-        document.querySelector(".wind").innerText=data.wind.speed+ " km/h"
-        document.querySelector(".weatherIcon").src=`./images/${data.weather[0].main}.png`
-        document.querySelector(".weather").style.display="block"
-        document.querySelector(".error").style.display="none"
+    catch(error){
+        console.log(error);
     }
-
+    
 }
 
-document.querySelector(".searchButton").addEventListener("click",(e)=>{
-    var cityName=document.querySelector(".searchInput").value;
+document.forms[0].addEventListener("submit",(e)=>e.preventDefault())
+
+document.querySelector(".searchButton").addEventListener("click",()=>{
+    let cityName=document.querySelector("#searchInput").value;
     checkWeather(cityName)
 })
 
-document.querySelector(".searchInput").addEventListener("keydown",(e)=>{
-    if(e.key==="Enter"){
-        var cityName=document.querySelector(".searchInput").value;
+document.querySelector("#searchInput").addEventListener("keydown",(e)=>{
+    if(e.key=="Enter"){
+        let cityName=document.querySelector("#searchInput").value;
         checkWeather(cityName)
     }
 })
-
-
-
-
